4e0857f11cd2efb8721b026379ca5214b9d0dfa3
Kavisha022 <kawisha.nilmani@lowcodeminds.com>
2025-05-09 16:48:26 +0530

    modified

--- usingmymodeltraining.py
+++ usingmymodeltraining.py
@@ -16,10 +16,10 @@ os.environ["STREAMLIT_DISABLE_FILE_WATCHING"] = "1"
 def load_model():
     try:
         tokenizer = T5Tokenizer.from_pretrained(
-            "./t5_qa_model"
+            "./t5_qa_model"  ##############################################
         )
         model = T5ForConditionalGeneration.from_pretrained(
-            "./t5_qa_model"
+            "./t5_qa_model"  ###############################################
         )
         return tokenizer, model
     except Exception as e:
@@ -56,7 +56,7 @@ def generate_answer(question):
     try:
         input_text = f"question: {question}"
         input_ids = tokenizer.encode(input_text, return_tensors="pt", truncation=True)
-        output_ids = model.generate(input_ids, max_length=64)
+        output_ids = model.generate(input_ids, max_length=128)  ############################################### 64 0r 128
         return tokenizer.decode(output_ids[0], skip_special_tokens=True)
     except Exception as e:
         return f"Error generating answer: {str(e)}"
